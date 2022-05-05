5a9b1d045111515f9af57fbf29ae4e5be16dcd84
Kenneth <k.owusu.me@gmail.com>
2022-05-05 17:01:49 +0000

    feat: topbar searchbox

---
+++ components/TopBar.js
@@ -0,0 +1,26 @@
+const TopBar = ()=>{
+  
+
+ return(
+    <div className="topbar">
+      
+      <div className="topbar-name">
+          <p class="topbar-company">Basilic Restaurant</p>
+      </div>
+    
+
+
+
+     <div className="topbar-search">
+        <input type="text" className="topbar-search-box" placeholder="search recipe"/>
+     </div>
+
+   </div>
+
+ )
+
+}
+
+
+
+export default TopBar;
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,13 +6,29 @@ import styles from '../styles/Home.module.css'
 
 //=================components=========================//
 import LeftSidebar from '../components/LeftSidebar';
+import TopBar from '../components/TopBar';
 
 const Home: NextPage = () => {
   return (
     <div className="page-container h-full">
 
+
+     <div className="left">
       <LeftSidebar/>
-    </div>
+     </div>
+
+
+      <div className="middle">
+        <TopBar/>
+      </div>
+      
+      <div className="right">
+
+      </div>
+
+      
+   </div>
+
   )
 }
 
