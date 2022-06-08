5d6f3d967cd5d50a0500d14cd92cd315bef44885
Kenneth <k.owusu.me@gmail.com>
2022-06-08 15:36:28 +0000

    topbar searchbox

--- components/TopBar.js
+++ components/TopBar.js
@@ -1,18 +1,19 @@
-const TopBar = ()=>{
-  
+import SearchIconSvg from '../public/icons/search.svg';
 
+
+
+const TopBar = ()=>{
  return(
     <div className="topbar">
       
-      <div className="topbar-name">
-          <p className="topbar-company">Basilic Restaurant</p>
-      </div>
     
-
 
 
      <div className="topbar-search">
         <input type="text" className="topbar-search-box" placeholder="search recipe"/>
+        <div className="topbar-search-icon">
+          <SearchIconSvg/>
+        </div>
      </div>
 
    </div>
