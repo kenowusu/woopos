461b7ddf2e18b071ab3839132b2f53d0f9be0ad5
Kenneth <k.owusu.me@gmail.com>
2022-06-09 13:30:40 +0000

    category food item

--- components/Category.js
+++ components/Category.js
@@ -6,18 +6,26 @@ const Category = ()=>{
         1,2,2,3,4,5,6,7
     ]) 
 
+    const [foodItems,setFoodItems] = useState([
+        1,2,3,4,5,6,7,3,4,5,2,5,2,
+    ])
+
 
 
     /*******selects category on click**/
     const selectCategory = (e)=>{
         const target = e.target;
-        e.target.classList.add('category-item__selected')
+        target.classList.add('category-item__selected')
     }
-
+    
+    const selectFoodItem = (e)=>{
+        const target = e.target;
+        target.classList.add('category-food-item__selected')
+    }
     return(
         <div className="category-container ">
 
-            <div className="category-head-container ">
+            <div className="category-head-container">
                 <div className="category-head">
                     <div className="category-head-one cursor-pointer">Choose Category</div>
                     <div className="category-head-two cursor-pointer">View All</div>
@@ -50,6 +58,50 @@ const Category = ()=>{
 
                 </div>
             </div>
+
+
+
+            {/******************category-food************/}
+            <div className="category-food-container">
+                <div className="category-food">
+
+
+                    {/* category-food-item */}
+                    <div className="category-food-item-container">
+                        
+                        {foodItems.map((foodItem)=>{
+                            return(
+                                <div className="category-food-item category-food-item__select" onClick={selectFoodItem}>
+
+                                    <div className="category-food-img">
+                                        <img src="https://img-global.cpcdn.com/recipes/5da646cc1c73a947/1200x630cq70/photo.jpg" alt="" />
+                                    </div>
+                                    <div className="category-food-name">Fried Rice</div>
+                                    <div className="category-food-price">GHS 15.00</div>
+                                    
+                                    {/* category-food-option */}
+                                    <div className="category-food-option">
+                                        <div className="category-food-option-options"></div>
+                                        <button className="category-food-option-btn">Add</button>
+                                    </div>
+                                    {/* category-food-option  ends */}
+                                </div>    
+                            )
+                        })}
+
+                        
+                    
+                    
+                    
+                    
+                    </div>
+                    {/* category-food-item ends */}
+
+
+
+                </div>
+            </div>
+            {/**************category-food**************/}
 
 
         </div>
