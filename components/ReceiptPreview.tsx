6142ea5364ade0166730a37fbed6827d67b6c1a5
Kenneth Owusu <k.owusu.me@gmail.com>
2022-09-11 07:59:54 +0000

    add date and time to receipt

--- components/ReceiptPreview.tsx
+++ components/ReceiptPreview.tsx
@@ -5,11 +5,18 @@ import { FoodContext } from "./contexts/foodContext"
 export const ReceiptPreview = ()=>{
     const {orders} = useContext(FoodContext);
 
-    console.log('lolo',orders)
+    const date = new Date();
+	const current_date =   date.getDate()  +"-" +(date.getMonth()+1) + "-" + date.getFullYear();
+    const  current_time = date.getHours()+":"+date.getMinutes()+":"+ date.getSeconds();
     return(
         <div>
             <div className="receipt" id="receipt">
-
+                
+                <div className="receipt-datetime">
+                    <div>{current_date}</div>
+                    <div>{current_time}</div>
+                </div>
+              
                 <div className="receipt-header">
                     <div className="receipt-img-cont dotted">
                         <div className="receipt-img">
@@ -25,7 +32,7 @@ export const ReceiptPreview = ()=>{
                     <table className=""style={{width:'100%'}}>
                         <thead className="receipt-head">
 
-                        <tr className="dotted">
+                        <tr className="">
                             <th>Qty</th>
                             <th>Item Description</th>
                             <th>Price</th>
