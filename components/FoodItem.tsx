f09ef8235a772cffbf607930ff00e0dd97c50905
Kenneth Owusu <k.owusu.me@gmail.com>
2022-09-20 14:25:56 +0000

    change food ui

--- components/FoodItem.tsx
+++ components/FoodItem.tsx
@@ -5,20 +5,22 @@ import Image from "next/image";
 export const FoodItem = ({food,addToOrder})=>{
     
     return(
-        <div className="category-food-item category-food-item__select"  >
+        <div className="food-item category-food-item__select"  >
 
-            <div className="category-food-img">
-            <Image src={food.images[0].src || "public/images/no-image.jpg"} width={204} height={120} />
+            {/* food item group */}
+            <div className="food-item-grp">
+                <div className="food-item-img">
+                <Image src={food.images[0].src || "public/images/no-image.jpg"} width={204} height={120} />
+                </div>
+                <div className="food-item-name">{food.name}</div>
+                <div className="food-item-price">GHS {food.on_sale ? food.sale_price :food.regular_price}</div>    
             </div>
-            <div className="category-food-name">{food.name}</div>
-            <div className="category-food-price">GHS {food.on_sale ? food.sale_price :food.regular_price}</div>
-            
+
             {/* category-food-option */}
-            <div className="category-food-option" style={{marginTop:"auto"}}>
-                <div className="category-food-option-options"></div>
-                <button className="category-food-option-btn" food_id={food.id} onClick={addToOrder}>Add</button>
+            <div className="food-item-option" style={{marginTop:"auto"}}>
+                <button className="food-item-btn" food_id={food.id} onClick={addToOrder}>Add</button>
             </div>
-            {/* category-food-option  ends */}
+       
         </div>    
     )
 }
