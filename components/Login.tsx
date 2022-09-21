b25d2404aa83754d43340fc70e055d8bf79fba88
Kenneth Owusu <k.owusu.me@gmail.com>
2022-09-21 16:47:34 +0000

    generate cookie when log in

--- components/Login.tsx
+++ components/Login.tsx
@@ -1,6 +1,41 @@
+import {useState} from 'react';
+import Cookies from 'universal-cookie';
+
+const cookies = new Cookies()
+
+const cashierUsername = process.env.NEXT_PUBLIC_cashier_username;
+const cashierPassword = process.env.NEXT_PUBLIC_cashier_password;
+
+
+
 export const Login = ()=>{
 
+    const [username,setUsername] = useState('');
+    const [password,setPassword] = useState('');
+    const [error,setError] = useState('');
 
+  
+
+    const login = (e)=>{
+        e.preventDefault()
+        
+        console.log(cookies)
+
+        if(username === cashierUsername && password === cashierPassword){
+
+            //username and password is correct 
+            //generate jwt and set cookie
+            const btn = document.getElementById('auth-login-btn');
+            btn.style.backgroundColor = "#fcdeea";
+            cookies.set('userauth','XKoFDBYHfr')
+            window.location.href = "/"
+        }else{
+           
+            
+            setError("invalid username or password")
+           
+        }
+    }
 
     return(
         <div className="auth">
@@ -8,15 +43,25 @@ export const Login = ()=>{
 
                 <div className="auth-container w-full">
                     
+                    <div className="auth-error">{error}</div>
                     <form className="auth-login-form w-full">
                         <div className="text">Welcome Back</div>
 
                         <div className="form-grp">
-                            <input type="text"  placeholder="Enter username"/>
-                            <input type="text" placeholder="Enter password"/>
+                            <input type="text"  placeholder="Enter username"
+                            value={username}
+                            onChange={(e)=>setUsername(e.target.value)}
+                            />
+                            <input type="text" placeholder="Enter password"
+                                value={password}
+                                onChange={(e)=>setPassword(e.target.value)}
+                            />
                         </div>
                         <div className="auth-login-submitbtn">
-                            <input type="submit" value="Sign In" />
+                            <input type="submit" value="Sign In" 
+                            id="auth-login-btn"
+                            onClick={login}
+                            />
                         </div>
                     
                     </form>
--- components/wc/config.js
+++ components/wc/config.js
@@ -4,7 +4,6 @@ const url = process.env.NEXT_PUBLIC_wc_url;
 const consumerKey = process.env.NEXT_PUBLIC_wc_consumer_key;
 const consumerSecret = process.env.NEXT_PUBLIC_wc_consumer_secret;
 
-
 //check if url,consumerKey and consumerSecret has value 
 if(!url || !consumerKey || !consumerSecret){
     console.error("Value for wc_url or wc_consumer_key or wc_consumer_secret missen in .env.local")
