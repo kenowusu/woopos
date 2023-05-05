11e9e38794cda33023ed6f28f30db0e13762706b
Kenneth Owusu <k.owusu.me@gmail.com>
2023-05-05 23:10:38 +0000

    clean up

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,56 +1,37 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-
-
-
+import type { NextPage } from "next";
+import Head from "next/head";
 
 /****************import *************/
-import api from '../components/wc/config';
-import { Login } from '../components/Login';
-
-
-
+import { Login } from "../components/Login";
 
 export async function getServerSideProps(context) {
   const cookies = context.req.cookies;
 
-  if(cookies.userauth && cookies.userauth == 'XKoFDBYHfr'){
+  if (cookies.userauth && cookies.userauth == "XKoFDBYHfr") {
     return {
       redirect: {
-        destination: '/',
+        destination: "/",
         permanent: false,
       },
-      props: {}
-    }
-  }
-  
-  return {
-    props: {}
+      props: {},
+    };
   }
 
-  
+  return {
+    props: {},
+  };
 }
 
 const LoginPage: NextPage = () => {
-
-
-
-
-
-
-
-
   return (
-
-  <>
-  <Head>
-    <link rel="icon" href="/images/favicon.ico" />
-    <title>Login Basilic Pos</title>
-
-  </Head>
-    <Login/>
-  </>
-  )
-}
+    <>
+      <Head>
+        <link rel="icon" href="/images/favicon.ico" />
+        <title>Login Basilic Pos</title>
+      </Head>
+      <Login />
+    </>
+  );
+};
 
 export default LoginPage;
